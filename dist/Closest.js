454efcc440da70c4cfaf9b72ef974e2bbb2690b1
Alexandre Neves <adn.denied@gmail.com>
2018-07-23 14:08:04 +0100

    build

--- dist/Closest.js
+++ dist/Closest.js
@@ -3,7 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var Closest = function Closest(el, selector) {
+var Closest = function Closest(el, selector, counter) {
+    counter = counter === undefined ? 0 : counter + 1;
+
     if (el === undefined || selector === undefined || el.tagName.toLowerCase() === 'body') {
         return false;
     }
@@ -11,11 +13,11 @@ var Closest = function Closest(el, selector) {
     var temp = selector.substr(1);
     var firstChar = selector.charAt(0);
 
-    if (firstChar === '#' && el.id === temp || firstChar === '.' && el.classList.contains(temp) || el.tagName.toLowerCase() === selector) {
+    if (firstChar === '#' && el.id === temp || firstChar === '.' && el.classList.contains(temp) || counter > 0 && el.tagName.toLowerCase() === selector) {
         return el;
     }
 
-    return Closest(el.parentNode, selector);
+    return Closest(el.parentNode, selector, counter);
 };
 
 exports.default = Closest;
